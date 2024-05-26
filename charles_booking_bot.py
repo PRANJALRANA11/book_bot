dee4a6d3e94c6dac152cc5d56753fa708b8e7cef
Pranjal Rana <129268721+PRANJALRANA11@users.noreply.github.com>
2024-05-26 10:58:07 +0530

    adding-browser

--- charles_booking_bot.py
+++ charles_booking_bot.py
@@ -22,7 +22,7 @@ def run_selenium_script(email, password):
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
 
 
     try:
